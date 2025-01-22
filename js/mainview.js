7bdc10f186e0f51ac4a4783abc2163e4a218dad3
esx11954 <42484592+esx11954@users.noreply.github.com>
2025-01-22 10:22:55 +0900

    delete mv, ov, htu proto

--- js/mainview.js
+++ js/mainview.js
@@ -1,35 +1,40 @@
 // scroll
 $(function () {
-    $(window).on("scroll", function () {
-        const sliderHeight = $("#sec-mainview").height();
+  $(window).on("scroll", function () {
+    // const sliderHeight = $("#sec-mainview").height();
+    const sliderHeight = $("#sec-nav1").height();
 
-        if ($("#sec-mainview").width() > 800){
-            if (sliderHeight < $(this).scrollTop()) {
-                $("#header").fadeIn();
-                $("#header").removeClass("s-header-hidden");
-            } else {
-                $("#header").fadeOut(400,function(){
-                    $("#header").addClass("s-header-hidden");
-                });
-            }
-        } else {
-            if (sliderHeight < $(this).scrollTop()) {
-                $("#header .s-header-inner").removeClass("s-header-inner-top");
-            } else {
-                $("#header .s-header-inner").addClass("s-header-inner-top");
-            }
-        }
-    });
+    console.log(sliderHeight);
 
-    $(".s-header-sp a[href]").on("click", function () {
-        $('.hamburger').trigger('click');
-    });
+    // if ($("#sec-mainview").width() > 800) {
+    if ($("#sec-nav1").width() > 800) {
+      console.log($(this).scrollTop());
+      if (sliderHeight < $(this).scrollTop()) {
+        $("#header").fadeIn();
+        $("#header").removeClass("s-header-hidden");
+      } else {
+        $("#header").fadeOut(400, function () {
+          $("#header").addClass("s-header-hidden");
+        });
+      }
+    } else {
+      if (sliderHeight < $(this).scrollTop()) {
+        $("#header .s-header-inner").removeClass("s-header-inner-top");
+      } else {
+        $("#header .s-header-inner").addClass("s-header-inner-top");
+      }
+    }
+  });
 
-    $("#check").on("change", function () {
-        if ($("html").css("overflow") === "hidden") {
-            $("html").css({ height: "", overflow: "" });
-        } else {
-            $("html").css({ height: "100%", overflow: "hidden" });
-        }
-    });
+  $(".s-header-sp a[href]").on("click", function () {
+    $(".hamburger").trigger("click");
+  });
+
+  $("#check").on("change", function () {
+    if ($("html").css("overflow") === "hidden") {
+      $("html").css({ height: "", overflow: "" });
+    } else {
+      $("html").css({ height: "100%", overflow: "hidden" });
+    }
+  });
 });
